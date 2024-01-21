2fdff29298f0404c3667ec0020657383421bb80d
msy <msy@withfirst.com>
2024-01-21 22:32:39 +0900

    feat: design

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section></section>
+  <main id="default">
+    <LayoutNavbar />
+    <div id="main">
+      <slot />
+    </div>
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#default {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  #navigation {
+    width: 100%;
+    max-width: 300px;
+  }
+  #main {
+    flex: 1 1 0%;
+  }
+}
+</style>
