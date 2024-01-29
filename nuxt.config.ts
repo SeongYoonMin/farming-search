2b20c919b5ab9ae49895e59d5cfe5bc52df4a67a
SeongYoonMin <namo4862@naver.com>
2024-01-30 01:26:07 +0900

    feat: pretendard fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          as: "style",
+          href: "https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css",
+        },
+      ],
+    },
     pageTransition: { name: "page", mode: "out-in" },
   },
   css: ["~/assets/css/reset.css", "~/assets/scss/main.scss"],
